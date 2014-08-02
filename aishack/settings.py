65d51f53aa5536b4c19f7885198b3e56a154f576
Utkarsh Sinha <sinha.utkarsh1990@gmail.com>
2014-08-02 12:35:18 +0530

    Serve static files during dev

--- aishack/settings.py
+++ aishack/settings.py
@@ -79,4 +79,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATICFILES_DIRS =(os.path.join(BASE_DIR, "static"),)
+STATICFILES_FINDERS = ("django.contrib.staticfiles.finders.FileSystemFinder",
+                        "django.contrib.staticfiles.finders.AppDirectoriesFinder")
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
--- aishack/urls.py
+++ aishack/urls.py
@@ -1,12 +1,20 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
-from django.contrib import admin
-admin.autodiscover()
+import settings
+
+# Custom modules
+from aishack import views
+
+# Uncomment the next two lines to enable the admin
+# from django.contrib import admin
+# admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'aishack.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index),
+) + static(settings.STATIC_URL)
