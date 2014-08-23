ffa1a727d7f2760f88364d41546f475d9564423a
Utkarsh Sinha <sinha.utkarsh1990@gmail.com>
2014-08-23 20:16:52 +0530

    Back to top button

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,4 +1,12 @@
+function gotoTop() {
+    var targetOffset = $('body').offset().top;
+    $('html,body').animate({scrollTop: targetOffset}, 1000);
+    return false;
+}
+
 $(document).ready(function() {
     $("#btn-signup").tooltip({placement: 'right', title: 'Signup, track your progress, take quizzes.'});
     $('.carousel').carousel();
+
+    $('.to-top').click(gotoTop);
 });
