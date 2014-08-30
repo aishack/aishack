d29ccc13ef506c43434b76ff66aa3e0c4b8e102a
Utkarsh Sinha <sinha.utkarsh1990@gmail.com>
2014-08-30 13:28:52 +0530

    Serve static images for admin

--- aishack/settings.py
+++ aishack/settings.py
@@ -87,7 +87,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS =(os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = '/work/aishack/static/'
+# STATICFILES_DIRS =(os.path.join(BASE_DIR, "static"),)
 STATICFILES_FINDERS = ("django.contrib.staticfiles.finders.FileSystemFinder",
                         "django.contrib.staticfiles.finders.AppDirectoriesFinder")
 
