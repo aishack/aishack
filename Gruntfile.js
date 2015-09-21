b921cfd6f6d9b9af10c2e00dbde7d59273d527d9
Utkarsh Sinha <sinha.utkarsh1990@gmail.com>
2015-09-21 15:07:04 -0400

    Setup grunt for minifying and uglifying

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+var scssFiles = {'./aishack/static/css/custom.css': './aishack/static/css/custom.scss'};
+
+var cssFiles = ['./aishack/static/css/bootstrap.orange.min.css',
+                './aishack/static/css/bootstrap-editable.css',
+                './aishack/static/css/custom.css'];
+
+var coffeeFiles = ['./aishack/static/js/custom.coffee'];
+
+var jsFiles = ['./aishack/static/js/jquery-1.11.1.min.js',
+               './aishack/static/js/bootstrap.min.js',
+               './aishack/static/js/bootstrap-editable.min.js',
+               './aishack/static/js/custom.js'];
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    sass: {
+        options: {
+            sourceMap: false
+        },
+        dist: {
+            files: scssFiles
+        }
+    },
+    cssmin: {
+        options: {
+            shorthandCompacting: false,
+            roundingPrecision: -1
+        },
+        target: {
+            files: {
+                './aishack/static/css/styles.min.css': cssFiles
+            }
+        }
+    },
+    uglify: {
+        my_target: {
+            files: {
+                './aishack/static/js/output.min.js': jsFiles
+            }
+        }
+    },
+    coffee: {
+        compile: {
+            files: {
+                './aishack/static/js/custom.js': coffeeFiles
+            }
+        },
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+
+  grunt.registerTask('default', ['sass', 'cssmin', 'coffee', 'uglify']);
+};
