5c1ff1e6a2cad91a96287405892b7f81e17875cb
Utkarsh Sinha <sinha.utkarsh1990@gmail.com>
2015-11-14 20:32:50 -0500

    Make OpenCV Blueprints visible

--- aishack/knobs.py
+++ aishack/knobs.py
@@ -1,4 +1,4 @@
-show_opencv_blueprints = False
+show_opencv_blueprints = True
 show_vision_scrolls = False
 
 title_keywords = "Tutorials for OpenCV, computer vision, deep learning, image processing, neural networks and artificial intelligence."
