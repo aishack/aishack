8c41940f6873c8c47c5f25db2fb4dc0f4b3efe0f
Utkarsh Sinha <sinha.utkarsh1990@gmail.com>
2020-05-02 17:47:50 +0000

    Attempting to pass environment variables.

--- aishack/wsgi.py
+++ aishack/wsgi.py
@@ -10,5 +10,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aishack.settings")
 
+print('Starting with environment variables:')
+for var, value in os.environ.items():
+    print('%s: %s' % (var, value))
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
--- prod.sh
+++ prod.sh
@@ -3,5 +3,6 @@ echo Starting AI Shack prod server
 source env_prod/bin/activate
 SHA1=`git rev-parse HEAD`
 export SHA1
+echo $SHA1
 source env_prod/bin/activate
-uwsgi --ini $PWD/aishack_uwsgi.ini
+exec env SHA1=$SHA1 uwsgi --env SHA1=$SHA1 --ini $PWD/aishack_uwsgi.ini
